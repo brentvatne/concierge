89505bd0be7fe93d76a8254a65f0cb0f71b77fa0
Brent Vatne <brent.vatne@madriska.com>
2015-03-25 18:12:38 -0700

    Gotta have that current user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
 
   def home
-    @upcoming_bookings = current_user.bookings.upcoming_or_active.order('time ASC')
-    @upcoming_bookings.to_a.map! { |b| BookingSerializer.new(b, root: false) }
+    if current_user.present?
+      @upcoming_bookings = current_user.bookings.upcoming_or_active.order('time ASC')
+      @upcoming_bookings.to_a.map! { |b| BookingSerializer.new(b, root: false) }
+    end
   end
 
 end
